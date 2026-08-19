51c42b93c2e4fbf66080208a55dfedc24527fbc8
doxingxin <58932492+doxingxin@users.noreply.github.com>
2026-08-19 15:50:43 +0800

    Modify ATH10K debug settings in diy-part2.sh
    Updated ATH10K debug configuration settings and added success message.

--- build/p2w_r619ac-128m/diy‑part2.sh
+++ build/p2w_r619ac-128m/diy‑part2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd ${GITHUB_WORKSPACE}/openwrt || exit 1
-echo "==== diy-part2.sh ATH10K config modify ===="
-./scripts/config --set-val ATH10K_DEBUG n
-./scripts/config --set-val ATH10K_DEBUGFS n
-echo "==== 打印ATH10K配置 ===="
-grep ATH10K .config
+set -e
+cd ${GITHUB_WORKSPACE}/openwrt
+
+# 关闭ath10k debug，解决 list_count_nodes API不匹配报错
+./scripts/config --set-val CONFIG_ATH10K_DEBUG n
+./scripts/config --set-val CONFIG_ATH10K_DEBUGFS n
+
+echo "✅ diy-part2.sh: 已关闭ath10k debug调试模块"
